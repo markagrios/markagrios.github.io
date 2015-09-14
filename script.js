25d8d8cfd3d47dde8891816af0891698b18039b2
Mark Agrios <magrios@gmail.com>
2015-09-14 03:21:52 -0400

    made toggle symbols on the top dock

--- script.js
+++ script.js
@@ -14,12 +14,30 @@
 	}
 	
 	<!-- DOCK BUTTONS -->
+	var aboutmeclick = false;
+	var pagesclick = false;
+	var sayhiclick = false;
+	var projectsclick = false;
 	$("#aboutme").click(
 		function() {
 			$("#pagescontent").fadeOut(0);
 			$("#sayhibtncontent").fadeOut(0);
 			$("#projectscontent").fadeOut(0);
 			$("#aboutmecontent").slideToggle(400);
+			document.getElementById('pages').innerHTML = "pages";
+			document.getElementById('projects').innerHTML = "projects";
+			document.getElementById('sayhi').innerHTML = "say hi";
+
+			if(aboutmeclick == false) {
+				document.getElementById('aboutme').innerHTML = "about me &#9776;";
+				aboutmeclick = true;
+				pagesclick = false;
+				sayhiclick = false;
+				projectsclick = false;
+			} else {
+				document.getElementById('aboutme').innerHTML = "about me";
+				aboutmeclick = false;				
+			}			
 		}
 	);
 	$("#pages").click(
@@ -28,6 +46,20 @@
 			$("#sayhibtncontent").fadeOut(0);
 			$("#projectscontent").fadeOut(0);
 			$("#pagescontent").slideToggle(400);
+			document.getElementById('aboutme').innerHTML = "about me";
+			document.getElementById('projects').innerHTML = "projects";
+			document.getElementById('sayhi').innerHTML = "say hi";
+			
+			if(pagesclick == false) {
+				document.getElementById('pages').innerHTML = "pages &#9776;";
+				pagesclick = true;
+				aboutmeclick = false;
+				sayhiclick = false;
+				projectsclick = false;
+			} else {
+				document.getElementById('pages').innerHTML = "pages";
+				pagesclick = false;				
+			}
 		}
 	);
 	$("#sayhi").click(
@@ -36,6 +68,20 @@
 			$("#pagescontent").fadeOut(0);
 			$("#projectscontent").fadeOut(0);
 			$("#sayhibtncontent").slideToggle(400);
+			document.getElementById('pages').innerHTML = "pages";
+			document.getElementById('projects').innerHTML = "projects";
+			document.getElementById('aboutme').innerHTML = "about me";
+			
+			if(sayhiclick == false) {
+				document.getElementById('sayhi').innerHTML = "say hi &#9776;";
+				sayhiclick = true;
+				aboutmeclick = false;
+				sayhiclick = false;
+				projectsclick = false;
+			} else {
+				document.getElementById('sayhi').innerHTML = "say hi";
+				sayhiclick = false;				
+			}
 		}
 	);
 	$("#projects").click(
@@ -44,6 +90,20 @@
 			$("#pagescontent").fadeOut(0);
 			$("#sayhibtncontent").fadeOut(0);
 			$("#projectscontent").slideToggle(400);
+			document.getElementById('pages').innerHTML = "pages";
+			document.getElementById('aboutme').innerHTML = "about me";
+			document.getElementById('sayhi').innerHTML = "say hi";
+
+			if(projectsclick == false) {
+				document.getElementById('projects').innerHTML = "projects &#9776;";
+				projectsclick = true;
+				pagesclick = false;
+				sayhiclick = false;
+				aboutmeclick = false;				
+			} else {
+				document.getElementById('projects').innerHTML = "projects";
+				projectsclick = false;				
+			}
 		}
 	);
 	<!-- ------------ -->
