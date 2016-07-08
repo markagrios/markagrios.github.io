9640e60f043e0bbd24cca4005d719ae427d89d86
Mark Agrios <magrios@gmail.com>
2016-07-08 16:01:39 -0700

    new minimalist

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 console.log("script.js");
 
-//alert("markagrios.net's marverlous makeover is taking place so bear with me as I rebuild my website over the next few weeks.");
-
-//setTimeout to the three circle for an entrance animation
+alert("markagrios.net's marverlous makeover is taking place so bear with me as I rebuild my website over the next few weeks.");
