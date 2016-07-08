1383eb561e6b06b43cb969d3475ffaada7ddff47
Mark Agrios <magrios@gmail.com>
2016-07-08 15:08:48 -0700

    new layout, lets try it.

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ console.log("script.js");
 
 //alert("markagrios.net's marverlous makeover is taking place so bear with me as I rebuild my website over the next few weeks.");
 
+//setTimeout to the three circle for an entrance animation
