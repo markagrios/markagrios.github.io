15bb6f5a5f1008a81bdf242b58de215b4ecffd00
Mark Agrios <magrios@gmail.com>
2016-07-09 16:11:55 -0700

    really enjoying this

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 console.log("script.js");
 
-alert("markagrios.net's marverlous makeover is taking place so bear with me as I rebuild my website over the next few weeks.");
+//alert("markagrios.net's marverlous makeover is taking place so bear with me as I rebuild my website over the next few weeks.");
+
+$(".about").click(
+	function() {
+		$("#about-content").fadeToggle(700);
+		$(".lookdown").fadeToggle(700);
+	}
+);
