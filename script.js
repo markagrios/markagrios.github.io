6bec672ca6e199455211c92fa693cf9e2c30242d
Mark Agrios <magrios@gmail.com>
2016-07-09 18:46:54 -0700

    hopefully the look down button shouldn't move stuff around.

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 console.log("script.js");
 
-//alert("markagrios.net's marverlous makeover is taking place so bear with me as I rebuild my website over the next few weeks.");
+alert("markagrios.net's marverlous makeover is taking place so bear with me as I rebuild my website over the next few weeks.");
 
 $(".about").click(
 	function() {
 		$("#about-content").fadeToggle(700);
-		$(".lookdown").fadeToggle(700);
+		$(".lookdown").fadeIn(700);
+	}
+);
+
+$(".projects").click(
+	function() {
+		$("#projects-content").fadeToggle(700);
+		$(".lookdown").fadeIn(700);
+	}
+);
+
+$(".sayhi").click(
+	function() {
+		$("#sayhi-content").fadeToggle(700);
+		$(".lookdown").fadeIn(700);
 	}
 );
