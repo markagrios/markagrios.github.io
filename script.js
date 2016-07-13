b9b5120d61e625901471af3c7913af6870c927b8
Mark Agrios <magrios@gmail.com>
2016-07-12 20:55:57 -0700

    spelled marvelous correctly, thanks Elizabeth

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("script.js");
 
-alert("markagrios.net's marverlous makeover is taking place so bear with me as I rebuild my website over the next few weeks.");
+alert("markagrios.net's marvelous makeover is taking place so bear with me as I rebuild my website over the next few weeks.");
 
 $(".about").click(
 	function() {
