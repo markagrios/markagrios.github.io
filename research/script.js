e54679147abe1c4cf3d04f42642f1cfeab4e2c61
Mark Agrios <magrios@gmail.com>
2020-03-19 16:36:14 -0500

    MORE LINKS

--- research/script.js
+++ research/script.js
@@ -1,1 +1,20 @@
 console.log("script.js");
+
+function get_by_class(this_class) {
+  var c = document.getElementsByClassName(this_class);
+  return(c)
+}
+
+
+// randomize links
+link_options = ["subtle-link", "line-link", "flashy-link"];
+links = get_by_class("link");
+
+for (i=0;i<links.length;i++) {
+  choice = Math.floor(Math.random()*(link_options.length));
+  links[i].classList.add(link_options[choice]);
+}
+
+
+// console.log(get_by_class("content"))
+// get_by_class("content")
