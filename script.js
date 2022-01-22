3bf1ee8f556914c46ba3db2284d9bd1afa2a70f0
Mark Agrios <magrios@gmail.com>
2022-01-21 20:43:00 -0600

    update sheaves and CV

--- script.js
+++ script.js
@@ -32,23 +32,23 @@ fonts = ['Carter One',
 
 $("#sig_black").mouseenter(
 	function() {
-			$("#sig_c").fadeIn(400);
-			$("#sig_m").fadeIn(600);
-			$("#sig_y").fadeIn(800);
+			$("#sig_c").fadeIn(200);
+			$("#sig_m").fadeIn(400);
+			$("#sig_y").fadeIn(600);
 	}
 );
 
 $("#sig_black").mouseleave(
 	function() {
-			$("#sig_c").fadeOut(800);
-			$("#sig_m").fadeOut(600);
-			$("#sig_y").fadeOut(400);
+			$("#sig_c").fadeOut(600);
+			$("#sig_m").fadeOut(400);
+			$("#sig_y").fadeOut(200);
 	}
 );
 
 $("#sig_black").click(
 	function() {
-			$("#bar").fadeIn(500);
+			$("#bar").fadeIn(300);
 	}
 );
 
